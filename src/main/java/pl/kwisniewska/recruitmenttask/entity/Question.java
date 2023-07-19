52af8c4c3be63e262345271b2496a91904c0fe2f
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-07-19 12:34:13 +0200

    Add Database schema

---
+++ src/main/java/pl/kwisniewska/recruitmenttask/entity/Question.java
@@ -0,0 +1,31 @@
+package pl.kwisniewska.recruitmenttask.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class Question {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "api_id")
+  private Long apiId;
+
+  private String question;
+
+  @OneToMany
+  @JoinColumn(name = "question_id")
+  private List<Answer> answers = new ArrayList<>();
+}
