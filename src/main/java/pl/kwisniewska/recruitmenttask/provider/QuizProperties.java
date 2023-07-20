e0440987dffb455a799fe72d4781a750665fed7e
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-07-20 11:48:41 +0200

    Update quiz properties simplified

--- src/main/java/pl/kwisniewska/recruitmenttask/provider/QuizProperties.java
+++ src/main/java/pl/kwisniewska/recruitmenttask/provider/QuizProperties.java
@@ -6,24 +6,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Component
 @ConfigurationProperties("quiz-api")
 @PropertySource("classpath:application.yaml")
 @Getter
 @Setter
-public class QuizApiProperties {
+public class QuizProperties {
 
   private String url;
 
-  private List<Param> params = new ArrayList<>();
-
-  @Getter
-  @Setter
-  private class Param {
-    private String name;
-    private String value;
-  }
 }
