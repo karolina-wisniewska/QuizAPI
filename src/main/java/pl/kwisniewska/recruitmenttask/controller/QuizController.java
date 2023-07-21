12819886bc4c3a9e7354f07f2adb4111ab813523
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-07-21 11:02:02 +0200

    Update /api/questions: working properly

--- src/main/java/pl/kwisniewska/recruitmenttask/controller/QuizController.java
+++ src/main/java/pl/kwisniewska/recruitmenttask/controller/QuizController.java
@@ -1,17 +1,32 @@
 package pl.kwisniewska.recruitmenttask.controller;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.kwisniewska.recruitmenttask.converter.EntityToDtoConverter;
+import pl.kwisniewska.recruitmenttask.entity.Question;
+import pl.kwisniewska.recruitmenttask.model.ShowQuestionDto;
+import pl.kwisniewska.recruitmenttask.service.QuestionService;
 
 @RestController
+@Transactional
+@RequiredArgsConstructor
 @RequestMapping(value = "/api")
 public class QuizController {
 
+  private final QuestionService questionService;
+  private final EntityToDtoConverter converter;
+
   @GetMapping(value = "/questions")
-  public String getRandomQuestion(){
-    return "Random question";
+  public ResponseEntity<ShowQuestionDto> getRandomQuestion(){
+    Question randomQuestion = questionService.getRandom().get();
+    ShowQuestionDto showQuestionDto = converter.convertQuestionToQuizQuestionDto(randomQuestion);
+    return new ResponseEntity<>(showQuestionDto, HttpStatus.OK);
   }
 
   @PostMapping(value = "/answers")
