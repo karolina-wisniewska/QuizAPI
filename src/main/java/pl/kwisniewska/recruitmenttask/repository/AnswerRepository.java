d56735b5b3ebd7d10d8b380ab41dec94d6d5697d
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-07-21 22:13:38 +0200

    Add findAnswerByQuestionId

--- src/main/java/pl/kwisniewska/recruitmenttask/repository/AnswerRepository.java
+++ src/main/java/pl/kwisniewska/recruitmenttask/repository/AnswerRepository.java
@@ -1,10 +1,16 @@
 package pl.kwisniewska.recruitmenttask.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.kwisniewska.recruitmenttask.entity.Answer;
+
+import java.util.Set;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
+  @Query("select a from Question q join q.answers a where q.id=:questionId")
+  Set<Answer> findAnswersByQuestionId(@Param("questionId") Long questionId);
 }
--- src/main/java/pl/kwisniewska/recruitmenttask/service/AnswerService.java
+++ src/main/java/pl/kwisniewska/recruitmenttask/service/AnswerService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import pl.kwisniewska.recruitmenttask.entity.Answer;
 import pl.kwisniewska.recruitmenttask.repository.AnswerRepository;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class AnswerService {
@@ -14,4 +17,12 @@ public class AnswerService {
   public Answer save(Answer answer){
     return answerRepository.save(answer);
   }
+
+  public Set<Answer> findCorrectAnswersByQuestionId(Long questionId){
+    Set<Answer> allAnswers = answerRepository.findAnswersByQuestionId(questionId);
+    return allAnswers.stream()
+            .filter(a -> a.isCorrect())
+            .collect(Collectors.toSet());
+  }
+
 }
