59ba0fa6104a23c2a7760f115261fe1b6845bb85
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-07-21 11:49:32 +0200

    Update refactoring

--- src/main/java/pl/kwisniewska/recruitmenttask/controller/QuizController.java
+++ src/main/java/pl/kwisniewska/recruitmenttask/controller/QuizController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.kwisniewska.recruitmenttask.converter.EntityToDtoConverter;
 import pl.kwisniewska.recruitmenttask.entity.Question;
-import pl.kwisniewska.recruitmenttask.model.ShowQuestionDto;
+import pl.kwisniewska.recruitmenttask.model.AnswerToCheckDto;
+import pl.kwisniewska.recruitmenttask.model.QuestionToShowDto;
 import pl.kwisniewska.recruitmenttask.service.QuestionService;
 
 @RestController
@@ -23,15 +24,15 @@ public class QuizController {
   private final EntityToDtoConverter converter;
 
   @GetMapping(value = "/questions")
-  public ResponseEntity<ShowQuestionDto> getRandomQuestion(){
+  public ResponseEntity<QuestionToShowDto> getRandomQuestion(){
     Question randomQuestion = questionService.getRandom().get();
-    ShowQuestionDto showQuestionDto = converter.convertQuestionToQuizQuestionDto(randomQuestion);
-    return new ResponseEntity<>(showQuestionDto, HttpStatus.OK);
+    QuestionToShowDto questionToShowDto = converter.convertQuestionToQuizQuestionDto(randomQuestion);
+    return new ResponseEntity<>(questionToShowDto, HttpStatus.OK);
   }
 
   @PostMapping(value = "/answers")
-  public String answerQuestion(){
+  public String answerQuestion(AnswerToCheckDto answerToCheckDto){
+
     return "My answer";
   }
-
 }
--- src/main/java/pl/kwisniewska/recruitmenttask/converter/EntityToDtoConverter.java
+++ src/main/java/pl/kwisniewska/recruitmenttask/converter/EntityToDtoConverter.java
@@ -3,8 +3,8 @@ package pl.kwisniewska.recruitmenttask.converter;
 import org.springframework.stereotype.Component;
 import pl.kwisniewska.recruitmenttask.entity.Answer;
 import pl.kwisniewska.recruitmenttask.entity.Question;
-import pl.kwisniewska.recruitmenttask.model.ShowAnswerDto;
-import pl.kwisniewska.recruitmenttask.model.ShowQuestionDto;
+import pl.kwisniewska.recruitmenttask.model.AnswerToShowDto;
+import pl.kwisniewska.recruitmenttask.model.QuestionToShowDto;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,22 +12,22 @@ import java.util.stream.Collectors;
 @Component
 public class EntityToDtoConverter {
 
-  private ShowAnswerDto convertAnswerToQuizAnswerDto(Answer answer) {
-    ShowAnswerDto showAnswerDto = new ShowAnswerDto();
-    showAnswerDto.setId(answer.getId());
-    showAnswerDto.setAnswer(answer.getAnswer());
-    return showAnswerDto;
+  private AnswerToShowDto convertAnswerToQuizAnswerDto(Answer answer) {
+    AnswerToShowDto answerToShowDto = new AnswerToShowDto();
+    answerToShowDto.setId(answer.getId());
+    answerToShowDto.setAnswer(answer.getAnswer());
+    return answerToShowDto;
   }
 
-  public ShowQuestionDto convertQuestionToQuizQuestionDto(Question question) {
-    ShowQuestionDto showQuestionDto = new ShowQuestionDto();
-    showQuestionDto.setId(question.getId());
-    showQuestionDto.setQuestion(question.getQuestion());
+  public QuestionToShowDto convertQuestionToQuizQuestionDto(Question question) {
+    QuestionToShowDto questionToShowDto = new QuestionToShowDto();
+    questionToShowDto.setId(question.getId());
+    questionToShowDto.setQuestion(question.getQuestion());
     List<Answer> answers = question.getAnswers();
-    List<ShowAnswerDto> answerDtos = answers.stream()
+    List<AnswerToShowDto> answerDtos = answers.stream()
             .map(answer -> convertAnswerToQuizAnswerDto(answer))
             .collect(Collectors.toList());
-    showQuestionDto.setAnswers(answerDtos);
-    return showQuestionDto;
+    questionToShowDto.setAnswers(answerDtos);
+    return questionToShowDto;
   }
 }
