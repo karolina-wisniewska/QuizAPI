bc012f38b52ffc6ae1b988b7a50e6daecfc1c7cf
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-07-21 10:54:44 +0200

    Update refactoring

--- src/main/java/pl/kwisniewska/recruitmenttask/RecruitmentTaskApplication.java
+++ src/main/java/pl/kwisniewska/recruitmenttask/RecruitmentTaskApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import pl.kwisniewska.recruitmenttask.converter.QuestionDtoToQuestionConverter;
+import pl.kwisniewska.recruitmenttask.converter.ProviderConverter;
 import pl.kwisniewska.recruitmenttask.entity.Question;
-import pl.kwisniewska.recruitmenttask.model.QuestionDto;
-import pl.kwisniewska.recruitmenttask.service.ProviderService;
+import pl.kwisniewska.recruitmenttask.provider.model.QuestionDto;
+import pl.kwisniewska.recruitmenttask.provider.service.ProviderService;
 import pl.kwisniewska.recruitmenttask.service.QuestionService;
 
 import java.util.stream.IntStream;
@@ -23,11 +23,11 @@ public class RecruitmentTaskApplication {
   }
 
   @Bean
-  CommandLineRunner runner(ProviderService providerService, QuestionDtoToQuestionConverter questionConverter, QuestionService questionService) {
+  CommandLineRunner runner(ProviderService providerService, ProviderConverter providerConverter, QuestionService questionService) {
     return args -> IntStream.range(0,10)
             .forEach(i -> {
               QuestionDto questionDto = providerService.getData();
-              Question question = questionConverter.convert(questionDto);
+              Question question = providerConverter.convertQuestionDtoToQuestion(questionDto);
               questionService.save(question);
             });
   }
