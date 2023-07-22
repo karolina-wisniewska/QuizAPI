5b789cefee04842da742867640b1f2818736cc22
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-07-22 12:24:07 +0200

    Add test annotation

--- src/test/java/pl/kwisniewska/recruitmenttask/RecruitmentTaskApplicationTests.java
+++ src/test/java/pl/kwisniewska/recruitmenttask/RecruitmentTaskApplicationTests.java
@@ -3,7 +3,7 @@ package pl.kwisniewska.recruitmenttask;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = RecruitmentTaskApplicationTests.class)
 class RecruitmentTaskApplicationTests {
 
   @Test
