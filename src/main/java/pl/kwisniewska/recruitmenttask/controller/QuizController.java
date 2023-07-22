eab2d8752e4395a07127683e8035e9589fd95bdf
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-07-22 10:35:24 +0200

    Add validation of parameters

--- src/main/java/pl/kwisniewska/recruitmenttask/controller/QuizController.java
+++ src/main/java/pl/kwisniewska/recruitmenttask/controller/QuizController.java
@@ -1,11 +1,16 @@
 package pl.kwisniewska.recruitmenttask.controller;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.kwisniewska.recruitmenttask.converter.EntityToDtoConverter;
@@ -15,6 +20,8 @@ import pl.kwisniewska.recruitmenttask.model.CheckDto;
 import pl.kwisniewska.recruitmenttask.model.QuestionToShowDto;
 import pl.kwisniewska.recruitmenttask.service.QuestionService;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 @RestController
@@ -27,20 +34,32 @@ public class QuizController {
   private final EntityToDtoConverter converter;
 
   @GetMapping(value = "/questions")
-  public ResponseEntity<QuestionToShowDto> getRandomQuestion(){
+  public ResponseEntity<QuestionToShowDto> getRandomQuestion() {
     Question randomQuestion = questionService.getRandom().get();
     QuestionToShowDto questionToShowDto = converter.convertQuestionToQuizQuestionDto(randomQuestion);
     return new ResponseEntity<>(questionToShowDto, HttpStatus.OK);
   }
 
   @PostMapping(value = "/answers")
-  public ResponseEntity<CheckDto> answerQuestion(AnswerToCheckDto answerToCheckDto){
+  public ResponseEntity<CheckDto> answerQuestion(@Valid @RequestBody AnswerToCheckDto answerToCheckDto) {
     Long questionId = answerToCheckDto.getQuestionId();
     Set<Long> answers = answerToCheckDto.getAnswers();
 
     boolean isPassed = questionService.isQuestionPassed(questionId, answers);
     CheckDto checkDto = converter.convertBooleanToCheckDto(isPassed);
 
-    return new ResponseEntity(checkDto, HttpStatus.OK);
+    return new ResponseEntity<>(checkDto, HttpStatus.OK);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleValidationExceptions(
+          MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
   }
 }
--- src/main/java/pl/kwisniewska/recruitmenttask/model/AnswerToCheckDto.java
+++ src/main/java/pl/kwisniewska/recruitmenttask/model/AnswerToCheckDto.java
@@ -1,5 +1,7 @@
 package pl.kwisniewska.recruitmenttask.model;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.Set;
@@ -7,6 +9,10 @@ import java.util.Set;
 @Data
 public class AnswerToCheckDto {
 
+  @NotEmpty
+  @NotNull
   private Set<Long> answers;
+
+  @NotNull
   private Long questionId;
 }
