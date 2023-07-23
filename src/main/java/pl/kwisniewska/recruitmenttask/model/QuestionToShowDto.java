559305ae39f08868144e8fda5d218ffff01f9da5
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-07-23 13:06:57 +0200

    Add constructors for testing purposes

--- src/main/java/pl/kwisniewska/recruitmenttask/model/QuestionToShowDto.java
+++ src/main/java/pl/kwisniewska/recruitmenttask/model/QuestionToShowDto.java
@@ -1,10 +1,14 @@
 package pl.kwisniewska.recruitmenttask.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class QuestionToShowDto {
 
   private Long id;
