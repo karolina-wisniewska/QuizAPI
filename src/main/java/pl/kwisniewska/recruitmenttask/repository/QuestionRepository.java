62cb620c95e5a98600294cdafeac0a4df19635f4
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-07-25 13:37:14 +0200

    Add EntityGraph

--- src/main/java/pl/kwisniewska/recruitmenttask/repository/QuestionRepository.java
+++ src/main/java/pl/kwisniewska/recruitmenttask/repository/QuestionRepository.java
@@ -1,10 +1,17 @@
 package pl.kwisniewska.recruitmenttask.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.kwisniewska.recruitmenttask.entity.Question;
 
+import java.util.Optional;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+  @Override
+  @EntityGraph(attributePaths = {"answers"})
+  Optional<Question> findById(Long id);
 
 }
